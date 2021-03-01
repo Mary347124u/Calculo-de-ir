99a4ec96d9489548ecb96b1791b6dcde40ea2367
Mary <marybsa135@gmail.com>
2021-02-28 22:14:52 -0300

    first commit

---
+++ PagarIR/MainForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 28/02/2021
+ * Time: 21:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PagarIR
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	     float salario=float.Parse(textBox1.Text);
+	     float ir=0;
+	                     if (salario <= 2500)
+                {
+                    textBox2.Text = "isento";
+                } else if (salario <= 5000)
+                {
+                    ir = salario * 0.10F;
+                    textBox2.Text = ir.ToString();
+                } 
+	            else if (salario < 10000 )
+                {
+                    ir = salario * 0.15F;
+                    textBox2.Text = ir.ToString();
+                }else
+                {
+                    ir = salario * 0.25F;
+                    textBox2.Text = ir.ToString();
+                }
+
+		}
+	}
+}
---
+++ PagarIR/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 28/02/2021
+ * Time: 21:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace PagarIR
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
